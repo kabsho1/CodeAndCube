3d0ac7b52a6406efec3d13ffc0916a290b9da2e4
Kabir <kabir.batra2003@gmail.com>
2021-02-17 17:50:27 -0800

    navbar revamp, added hamburger for mobile

--- js/script.js
+++ js/script.js
@@ -1,29 +1,43 @@
-// fetch("https://ipinfo.io/json")
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (myJson) {
-//     console.log(myJson);
-//     document.querySelector("#hours").innerHTML = myJson.ip;
-//   })
-//   .catch(function (error) {
-//     console.log("Error: " + error);
-//   });
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+const links = document.querySelectorAll('.nav-links li');
 
+const body = document.body;
 
-// let xhr = new XMLHttpRequest();
-// xhr.open('GET', "https://ipinfo.io/json", true);
-// xhr.send();
-//
-// xhr.addEventListener("readystatechange", processRequest, false);
-// function processRequest(e) {
-//     if (xhr.readyState == 4) {
-//       let response = JSON.parse(xhr.responseText);
-//       document.querySelector("#hours").innerHTML = xhr.responseText;
-//     }
-// }
-// document.querySelector(".totalHours").innerHTML = "";
+var menuOpen = false;
 
+const linksCount = 6;
+
+var linksDelays = [];
+for(var i = 1; i <= linksCount; i++) {
+  linksDelays.push(i * 0.2);
+}
+
+hamburger.addEventListener('click', () => {
+  if(!menuOpen) {
+    navLinks.classList.add('open');
+    menuOpen = true;
+    body.style.overflow = "hidden";
+  }
+  else {
+    navLinks.classList.remove('open');
+    menuOpen = false;
+    body.style.overflow = "visible";
+  }
+
+  hamburger.classList.toggle('open');
+
+  for(var i = 0; i < linksCount; i++) {
+    links[i].style.transition = `all 0.5s ease ${linksDelays[i]}s`;
+  }
+  for(var i = 0; i < linksCount; i++) {
+    linksDelays[i] = linksCount*0.2 - linksDelays[i];
+  }
+
+  links.forEach(link => {
+    link.classList.toggle('fade-in');
+  });
+});
 
 
 function goToHomePage() {
